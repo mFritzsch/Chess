ce8cf38409327a3c0a46afe8bbf47757616c4f4b
mFritzsch <mfritzsch89@gmail.com>
2018-12-04 19:50:47 +0100

    simplified some code

--- pieces.py
+++ pieces.py
@@ -1,67 +1,49 @@
 import pygame
 
 
-class WhiteKing(pygame.sprite.Sprite):
+class AnyPiece(pygame.sprite.Sprite):
+    def set_x_and_y(self, new_x, new_y):
+        self.rect.x = new_x
+        self.rect.y = new_y
+
+
+class WhiteKing(AnyPiece):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load("sprites/white_king.png")
         self.rect = self.image.get_rect()
 
-    def set_x_and_y(self, new_x, new_y):
-        self.rect.x = new_x
-        self.rect.y = new_y
 
-
-class WhiteQueen(pygame.sprite.Sprite):
+class WhiteQueen(AnyPiece):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load("sprites/white_queen.png")
         self.rect = self.image.get_rect()
 
-    def set_x_and_y(self, new_x, new_y):
-        self.rect.x = new_x
-        self.rect.y = new_y
 
-
-class WhiteBishop(pygame.sprite.Sprite):
+class WhiteBishop(AnyPiece):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load("sprites/white_bishop.png")
         self.rect = self.image.get_rect()
 
-    def set_x_and_y(self, new_x, new_y):
-        self.rect.x = new_x
-        self.rect.y = new_y
 
-
-class WhiteKnight(pygame.sprite.Sprite):
+class WhiteKnight(AnyPiece):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load("sprites/white_knight.png")
         self.rect = self.image.get_rect()
 
-    def set_x_and_y(self, new_x, new_y):
-        self.rect.x = new_x
-        self.rect.y = new_y
 
-
-class WhiteRook(pygame.sprite.Sprite):
+class WhiteRook(AnyPiece):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load("sprites/white_rook.png")
         self.rect = self.image.get_rect()
 
-    def set_x_and_y(self, new_x, new_y):
-        self.rect.x = new_x
-        self.rect.y = new_y
 
-
-class WhitePawn(pygame.sprite.Sprite):
+class WhitePawn(AnyPiece):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load("sprites/white_pawn.png")
         self.rect = self.image.get_rect()
-
-    def set_x_and_y(self, new_x, new_y):
-        self.rect.x = new_x
-        self.rect.y = new_y
